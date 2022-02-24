0d45e20b1a83b4cfed40edc170fcc04b22bb260d
chemacenturion <centurion.philadelphia@gmail.com>
2022-02-24 03:00:49 -0500

    stuck on setInterval and modal population for the night

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,24 @@
 const playBtn = document.querySelector("#generate");
+const refreshBtn = document.querySelector("#okay");
 
 // create function called generateNumbers
 function generateNumbers () {
     let randNum = document.getElementById('randNum');
-    let result = document.getElementById('result')
     // Number between 1 and 100,000
-    randNum.innerHTML = Math.floor(Math.random() * 100000) + 1;
+    setInterval(()=> {randNum.innerHTML = Math.floor(Math.random() * 100000) + 1}, 100);
+    clearInterval(handle);
+    handle = 0;
+    // upon finishing modal needs to populate by switching to active
         if(randNum.innerHTML > 1000) {
-            alert("This site says, 'Too bad! You've Lost!'")
+        // display "Too bad! You've Lost!"
         } else {
-            alert("This site says, 'Congratulations! You've Won!'")
+        // display "Hooray! You've Won!"
         }
 
 }
 
 // Add event listener to generate lottery numbers
 playBtn.addEventListener("click", () => playBtn.style.backgroundColor='aqua');
+playBtn.addEventListener("click", () => playBtn.style.border='2px solid skyblue');
 playBtn.addEventListener("click", generateNumbers);
+refreshBtn.addEventListener("click", () => refreshBtn.style.border='2px rgb(83, 83, 83) solid');
