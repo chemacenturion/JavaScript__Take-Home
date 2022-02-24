bc831edea1faa820b43e77b9f9962a9d2c853dd5
chemacenturion <centurion.philadelphia@gmail.com>
2022-02-24 11:30:31 -0500

    formatted with prettier

--- assets/script.js
+++ assets/script.js
@@ -6,68 +6,72 @@ const okBtn = document.querySelector("#okay");
 const closeBtn = document.querySelector("#close");
 const overlay = document.querySelector("#overlay");
 
-
 let message = "";
 let randResult = 0;
 
-resultModal.style.display="none"
-overlay.style.display="none"
+resultModal.style.display = "none";
+overlay.style.display = "none";
 
 // create function called generateNumbers
 function generateNumbers() {
-    let randNum = document.getElementById("randNum");
-    // Number between 1 and 100,000
-    // The animation of having the numbers spin was very hard to solve and my intitial inclination was setInterval, but I found this link eventually
-    // https://stackoverflow.com/questions/3583724/how-do-i-add-a-delay-in-a-javascript-loop
+  let randNum = document.getElementById("randNum");
+  // Number between 1 and 100,000
+  // The animation of having the numbers spin was very hard to solve and my intitial inclination was setInterval, but I found this link eventually
+  // https://stackoverflow.com/questions/3583724/how-do-i-add-a-delay-in-a-javascript-loop
 
-    var i = 1;                     //  set your counter to 1
+  var i = 1; //  set your counter to 1
 
-    function myLoop() {            //  create a loop function
-        setTimeout(function() {   //  call a 3s setTimeout when the loop is called
-            // console.log(" hello" + i);
-            randResult= (Math.floor(Math.random() * 100000) + 1)%100000;//[1-99999]
-            randNum.innerHTML=randResult;   //  your code here
-            i++;                             //  increment the counter
-            if (i < 10) {                    //  if the counter < 10, call the loop function
-                myLoop();                        //  ..  again which will trigger another 
-            } else {
-                updateMessage();
-            }                               //  ..  setTimeout()
-        }, 100)
+  function myLoop() {
+    //  create a loop function
+    setTimeout(function () {
+      //  call a 3s setTimeout when the loop is called
+      // console.log(" hello" + i);
+      randResult = (Math.floor(Math.random() * 100000) + 1) % 100000; //[1-99999]
+      randNum.innerHTML = randResult; //  your code here
+      i++; //  increment the counter
+      if (i < 10) {
+        //  if the counter < 10, call the loop function
+        myLoop(); //  ..  again which will trigger another
+      } else {
+        updateMessage();
+      } //  ..  setTimeout()
+    }, 100);
+  }
+  myLoop();
+
+  function updateMessage() {
+    if (randResult > 1000) {
+      message = "You've lost!";
+      //alert("You've lost!")
+    } else {
+      message = "You've won!";
     }
-    myLoop(); 
-
-    function updateMessage() {
-        if (randResult > 1000) {
-            message="You've lost!";
-            //alert("You've lost!")
-        } else {
-            message="You've won!";
-        }
-        successMessageRef.innerHTML=message; 
-        resultModal.style.display="block";
-        overlay.style.display="block";
-    }
-      
+    successMessageRef.innerHTML = message;
+    resultModal.style.display = "block";
+    overlay.style.display = "block";
+  }
 }
 
 playBtn.addEventListener("click", () => {
-    playBtn.style.backgroundColor='aqua';
-    playBtn.style.border='2px solid skyblue'; 
+  playBtn.style.backgroundColor = "aqua";
+  playBtn.style.border = "2px solid skyblue";
 });
 
-okBtn.addEventListener("click",() => {
-    resultModal.style.display="none";
-    overlay.style.display="none";
-})
+okBtn.addEventListener("click", () => {
+  resultModal.style.display = "none";
+  overlay.style.display = "none";
+});
 
-closeBtn.addEventListener("click",() => {
-    resultModal.style.display="none";
-    overlay.style.display="none";
-})
+closeBtn.addEventListener("click", () => {
+  resultModal.style.display = "none";
+  overlay.style.display = "none";
+});
 
 // Add event listener to generate lottery numbers
-playBtn.addEventListener("click", (generateNumbers));
+playBtn.addEventListener("click", generateNumbers);
 
 // Add event listener for modal buttons
-refreshBtn.addEventListener("click", () => refreshBtn.style.border='2px rgb(83, 83, 83) solid');
+refreshBtn.addEventListener(
+  "click",
+  () => (refreshBtn.style.border = "2px rgb(83, 83, 83) solid")
+);
